a800313e111fd01c33f952e2e8f3234708c579c0
Kobby Opoku <breezeexp1@gmail.com>
2022-12-12 22:48:10 +0000

    Create maven project

---
+++ src/main/java/de/allianz/project-10/Project10Application.java
@@ -0,0 +1,13 @@
+package de.allianz.project-10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project10Application.class, args);
+	}
+
+}
